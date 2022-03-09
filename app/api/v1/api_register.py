0f2c919beaa5a6b41b166a3084ab69c0015cb3d2
huyvu <vu.lh@teko.vn>
2022-03-09 14:29:01 +0700

    fix schema user

--- app/api/v1/api_register.py
+++ app/api/v1/api_register.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import Session
 
 from app.api import deps
 from app.schemas.sche_base import DataResponse
-from app.schemas.sche_user import UserDetail
+from app.schemas.sche_user import UserDetailRequest
 from app.services.srv_user import UserService
 
 router = APIRouter()
 
 
 @router.post('')
-def register(db: Session = Depends(deps.get_db), user: UserDetail = None):
+def register(db: Session = Depends(deps.get_db), user: UserDetailRequest = None):
     user = UserService.create_user(db=db, user=user)
     return DataResponse().success_response(data={'user_id': user.id})
--- app/schemas/sche_user.py
+++ app/schemas/sche_user.py
@@ -5,11 +5,18 @@ from app.schemas.sche_base import ItemBaseModel
 
 class UserDetail(ItemBaseModel):
     username: Optional[str]
-    password: Optional[str]
     first_name: Optional[str]
     last_name: Optional[str]
     email: Optional[str]
     phone_number: Optional[str]
+
+
+class UserDetailRequest(UserDetail):
+    password: Optional[str]
+
+
+class UserDetailResponse(UserDetail):
+    id: int
 
 
 
