c416bbcaab64ceb67f4ec9cf826524574cc47356
huyvu <vu.lh@teko.vn>
2022-03-23 15:42:19 +0700

    change params to body api update password

--- app/api/v1/api_user.py
+++ app/api/v1/api_user.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 
 from app.api import deps
 from app.helpers.login_manager import login_required
-from app.schemas.sche_base import DataResponse
+from app.schemas.sche_base import DataResponse, ItemBaseModel
 from app.schemas.sche_user import UserDetail, UserUpdateRequest
 from app.services.srv_user import UserService
 
 router = APIRouter()
+
+
+class UpdatePassword(ItemBaseModel):
+    current_password: str
+    update_password: str
 
 
 @router.get('/me', dependencies=[Depends(login_required)], response_model=DataResponse[UserDetail])
@@ -24,11 +29,8 @@ def update(current_user: UserDetail = Depends(UserService.get_current_user), db:
 
 @router.put('/me/password', dependencies=[Depends(login_required)])
 def update_password(current_user: UserDetail = Depends(UserService.get_current_user),
-                    db: Session = Depends(deps.get_db),
-                    current_password: str = None,
-                    update_password: str = None
-                    ):
-    user = UserService.update_password(db=db, current_password=current_password, update_password=update_password,
-                                       user_detail=current_user)
+                    db: Session = Depends(deps.get_db), password: UpdatePassword = None):
+    user = UserService.update_password(db=db, current_password=password.current_password,
+                                       update_password=password.update_password, user_detail=current_user)
     return DataResponse().success_response(data=user)
 
